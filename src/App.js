40fcb8557e6e60c930b2be1ba386a4c330a15a54
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-13 23:24:30 +0100

    Adding the main components in place

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { Layout } from 'antd';
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Navbar } from './components';
+import { Routes, Route, Link } from 'react-router-dom';
+import {
+  Navbar,
+  Home,
+  Exchanges,
+  News,
+  CryptoDetails,
+  Cryptos,
+} from './components';
 
 function App() {
   return (
@@ -9,7 +17,19 @@ function App() {
         <div className="navbar">
           <Navbar />
         </div>
-        <div className="main"></div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/exchanges" element={<Exchanges />} />
+                <Route path="/news" element={<News />} />
+                <Route path="/cryptos" element={<Cryptos />} />
+                <Route path="/crypto/:cryptoId" element={<CryptoDetails />} />
+              </Routes>
+            </div>
+          </Layout>
+        </div>
         <div className="footer"></div>
       </div>
     </>
