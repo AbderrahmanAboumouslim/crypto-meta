a19aab3e10028c639a71f96e713f2ca25bbd3f35
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-15 21:45:49 +0100

    working on Home page...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Col, Row, Statistic, Typography } from 'antd';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Typography.Title level={2} className="heading">
+        Universe of Cryptocurrencies
+      </Typography.Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Exchanges" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Market Cap" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h volume" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value={5} />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Home;
