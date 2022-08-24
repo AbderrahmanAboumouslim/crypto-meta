b1510b6047b5eb8f58e5611fc8794c7ff1f6b623
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-24 10:35:11 +0100

    change the previous code ( distracting stats )

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,9 @@ import { useCryptosQuery } from '../services/CryptoApi';
 
 const Home = () => {
   const { data, isFetching } = useCryptosQuery();
-  const {
-    totalCoins,
-    totalExchanges,
-    totalMarketCap,
-    totalMarkets,
-    total24hVolume,
-  } = data.data.stats;
+  const stats = data?.data?.stats;
+
+  if (isFetching) return console.log('Still loading...');
 
   return (
     <>
@@ -20,19 +16,19 @@ const Home = () => {
       </Typography.Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={totalCoins} />
+          <Statistic title="Total Cryptocurrencies" value={stats.totalCoins} />
         </Col>
         <Col span={12}>
-          <Statistic title="Exchanges" value={totalExchanges} />
+          <Statistic title="Exchanges" value={stats.totalExchanges} />
         </Col>
         <Col span={12}>
-          <Statistic title="Market Cap" value={totalMarketCap} />
+          <Statistic title="Market Cap" value={stats.totalMarketCap} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h volume" value={total24hVolume} />
+          <Statistic title="Total 24h volume" value={stats.total24hVolume} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={totalMarkets} />
+          <Statistic title="Total Markets" value={stats.totalMarkets} />
         </Col>
       </Row>
     </>
