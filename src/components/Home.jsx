06726d74eee5178a3a4efcf7deaaaa0068d64222
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-24 11:03:57 +0100

    adding titles with the read more

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Col, Row, Statistic, Typography } from 'antd';
 import { useCryptosQuery } from '../services/CryptoApi';
+import millify from 'millify';
+import Cryptos from './Cryptos';
+import News from './News';
 
 const Home = () => {
   const { data, isFetching } = useCryptosQuery();
@@ -22,15 +25,37 @@ const Home = () => {
           <Statistic title="Exchanges" value={stats.totalExchanges} />
         </Col>
         <Col span={12}>
-          <Statistic title="Market Cap" value={stats.totalMarketCap} />
+          <Statistic title="Market Cap" value={millify(stats.totalMarketCap)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h volume" value={stats.total24hVolume} />
+          <Statistic
+            title="Total 24h volume"
+            value={millify(stats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
           <Statistic title="Total Markets" value={stats.totalMarkets} />
         </Col>
       </Row>
+
+      <div className="home-heading-container">
+        <Typography.Title level={2} className="home-title">
+          Top 10 Cryptocurrencies
+        </Typography.Title>
+        <Typography.Title level={3} className="show-more">
+          <Link to="/cryptos">Read more</Link>
+        </Typography.Title>
+      </div>
+      <Cryptos simplified />
+      <div className="home-heading-container">
+        <Typography.Title level={2} className="home-title">
+          Hot Crypto News
+        </Typography.Title>
+        <Typography.Title level={3} className="show-more">
+          <Link to="/news">Read more</Link>
+        </Typography.Title>
+      </div>
+      <News simplified />
     </>
   );
 };
