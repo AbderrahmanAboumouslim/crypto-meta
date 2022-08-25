def30cbabf0267ff5587c213b45d4174b47497c8
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-25 12:34:00 +0100

    give a meaning to the card

--- src/components/Cryptos.jsx
+++ src/components/Cryptos.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Col, Row, Card, Input } from 'antd';
 import { useCryptosQuery } from '../services/CryptoApi';
 import { Link } from 'react-router-dom';
+import millify from 'millify';
 
 const Cryptos = () => {
   const { data: CryptoList, isFetching } = useCryptosQuery();
@@ -18,7 +19,9 @@ const Cryptos = () => {
                 extra={<img className="crypto-image" src={coin.iconUrl} />}
                 hoverable
               >
-                <p>{coin.name}</p>
+                <p>Price: {millify(coin.price)}</p>
+                <p>Market Cap: {millify(coin.marketCap)}</p>
+                <p>Daily Change: {millify(coin.change)}</p>
               </Card>
             </Link>
           </Col>
