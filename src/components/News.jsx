e35ddb76d217512a5fd4067d073adcaeb0e7b5a3
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-28 15:06:18 +0100

    Adding more stuff to our news cards

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,10 @@
-import { Col, Row, Typography, Card } from 'antd';
+import { Col, Row, Typography, Card, Avatar } from 'antd';
+import moment from 'moment';
 import React from 'react';
 import { useNewsQuery } from '../services/NewsApi';
 
+const standardImage =
+  'https://www.interactivebrokers.hu/images/web/cryptocurrency-hero.jpg';
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useNewsQuery({
     newsCategory: 'Cryptocurrency',
@@ -16,11 +19,41 @@ const News = ({ simplified }) => {
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
-            <a href={news.url} placeholder="_blank" rel="noreferrer">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <img
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: '1rem',
+                  marginBottom: '1rem',
+                }}
+                src={news?.image?.thumbnail?.contentUrl || standardImage}
+                alt="news"
+              />
               <div className="news-image-container">
                 <Typography.Title className="news-title" level={4}>
                   {news.name}
                 </Typography.Title>
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news?.provider[0]?.image?.thumbnail?.contentUrl ||
+                      standardImage
+                    }
+                    alt="news"
+                  />
+
+                  <Typography.Text>
+                    {moment(news.datePublished).startOf('ss').fromNow()}
+                  </Typography.Text>
+                </div>
               </div>
             </a>
           </Card>
