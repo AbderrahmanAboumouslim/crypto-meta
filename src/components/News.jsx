d272594c3e18be418bd2a0ffddb3b352db1bd14e
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-28 12:01:12 +0100

    Displaying the news data dynamically to the news components

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,14 +1,33 @@
+import { Col, Row, Typography, Card } from 'antd';
 import React from 'react';
 import { useNewsQuery } from '../services/NewsApi';
 
 const News = ({ simplified }) => {
-  const { data: news } = useNewsQuery({
+  const { data: cryptoNews } = useNewsQuery({
     newsCategory: 'Cryptocurrency',
-    count: simplified ? 10 : 100,
+    count: simplified ? 6 : 20,
   });
-  console.log(news);
+  console.log(cryptoNews);
 
-  return <div>News</div>;
+  if (!cryptoNews?.value) return console.log('Loading the news ...');
+
+  return (
+    <Row gutter={[25, 25]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} placeholder="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Typography.Title className="news-title" level={4}>
+                  {news.name}
+                </Typography.Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
