2143256aad39b4b4e25b9f74837e45843b755dae
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-09-12 23:03:31 +0100

    key

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -30,57 +30,59 @@ const CryptoDetails = () => {
 
   const test = millify(Number(coinData.price));
   console.log(test);
-  const stats = [
-    {
-      title: 'Price to USD',
-      value: `$ ${Number(coinData.price) && millify(Number(coinData.price))}`,
-      icon: <DollarCircleOutlined />,
-    },
-    { title: 'Rank', value: Number(coinData.rank), icon: <NumberOutlined /> },
-    {
-      title: '24h Volume',
-      value: `$ ${coinData.volume && millify(Number(coinData.volume))}`,
-      icon: <ThunderboltOutlined />,
-    },
-    {
-      title: 'Market Cap',
-      value: `$ ${coinData.marketCap && millify(Number(coinData.marketCap))}`,
-      icon: <DollarCircleOutlined />,
-    },
-    {
-      title: 'All-time-high(daily avg.)',
-      value: `$ ${millify(Number(coinData.allTimeHigh.price))}`,
-      icon: <TrophyOutlined />,
-    },
-  ];
+  // const stats = [
+  //   {
+  //     title: 'Price to USD',
+  //     value: `$ ${Number(coinData.price) && millify(Number(coinData.price))}`,
+  //     icon: <DollarCircleOutlined />,
+  //   },
+  //   { title: 'Rank', value: Number(coinData.rank), icon: <NumberOutlined /> },
+  //   {
+  //     title: '24h Volume',
+  //     value: `$ ${coinData.volume && millify(Number(coinData.volume))}`,
+  //     icon: <ThunderboltOutlined />,
+  //   },
+  //   {
+  //     title: 'Market Cap',
+  //     value: `$ ${coinData.marketCap && millify(Number(coinData.marketCap))}`,
+  //     icon: <DollarCircleOutlined />,
+  //   },
+  //   {
+  //     title: 'All-time-high(daily avg.)',
+  //     value: `$ ${millify(Number(coinData.allTimeHigh.price))}`,
+  //     icon: <TrophyOutlined />,
+  //   },
+  // ];
 
-  const genericStats = [
-    {
-      title: 'Number Of Markets',
-      value: coinData.numberOfMarkets,
-      icon: <FundOutlined />,
-    },
-    {
-      title: 'Number Of Exchanges',
-      value: coinData.numberOfExchanges,
-      icon: <MoneyCollectOutlined />,
-    },
-    {
-      title: 'Aprroved Supply',
-      value: coinData.approvedSupply ? <CheckOutlined /> : <StopOutlined />,
-      icon: <ExclamationCircleOutlined />,
-    },
-    {
-      title: 'Total Supply',
-      value: `$ ${millify(Number(coinData.totalSupply))}`,
-      icon: <ExclamationCircleOutlined />,
-    },
-    {
-      title: 'Circulating Supply',
-      value: `$ ${millify(Number(coinData.circulatingSupply))}`,
-      icon: <ExclamationCircleOutlined />,
-    },
-  ];
+  // const genericStats = [
+  //   {
+  //     title: 'Number Of Markets',
+  //     value: coinData.numberOfMarkets,
+  //     icon: <FundOutlined />,
+  //   },
+  //   {
+  //     title: 'Number Of Exchanges',
+  //     value: coinData.numberOfExchanges,
+  //     icon: <MoneyCollectOutlined />,
+  //   },
+  //   {
+  //     title: 'Aprroved Supply',
+  //     value: coinData.approvedSupply ? <CheckOutlined /> : <StopOutlined />,
+  //     icon: <ExclamationCircleOutlined />,
+  //   },
+  //   {
+  //     title: 'Total Supply',
+  //     value: `$ ${millify(Number(coinData.totalSupply))}`,
+  //     icon: <ExclamationCircleOutlined />,
+  //   },
+  //   {
+  //     title: 'Circulating Supply',
+  //     value: `$ ${millify(Number(coinData.circulatingSupply))}`,
+  //     icon: <ExclamationCircleOutlined />,
+  //   },
+  // ];
+
+  if (isFetching) return 'Still loading...';
 
   return (
     <>
