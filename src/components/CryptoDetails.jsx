88b62f46cf78fb6bda6956f6106f9552895871af
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-09-13 21:47:14 +0100

    Solve the problem

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -24,70 +24,73 @@ const CryptoDetails = () => {
   const coinData = data?.data?.coin;
   console.log(cryptoId);
   console.log(data);
-  console.log(coinData);
+
   console.log('*** the above 24 & 25 are log for cryptoDetails and data');
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
-  const test = millify(Number(coinData.price));
-  console.log(test);
-  // const stats = [
-  //   {
-  //     title: 'Price to USD',
-  //     value: `$ ${Number(coinData.price) && millify(Number(coinData.price))}`,
-  //     icon: <DollarCircleOutlined />,
-  //   },
-  //   { title: 'Rank', value: Number(coinData.rank), icon: <NumberOutlined /> },
-  //   {
-  //     title: '24h Volume',
-  //     value: `$ ${coinData.volume && millify(Number(coinData.volume))}`,
-  //     icon: <ThunderboltOutlined />,
-  //   },
-  //   {
-  //     title: 'Market Cap',
-  //     value: `$ ${coinData.marketCap && millify(Number(coinData.marketCap))}`,
-  //     icon: <DollarCircleOutlined />,
-  //   },
-  //   {
-  //     title: 'All-time-high(daily avg.)',
-  //     value: `$ ${millify(Number(coinData.allTimeHigh.price))}`,
-  //     icon: <TrophyOutlined />,
-  //   },
-  // ];
+  const abdo = {
+    title: 'Price to USD',
+    value: coinData.price,
+  };
 
-  // const genericStats = [
-  //   {
-  //     title: 'Number Of Markets',
-  //     value: coinData.numberOfMarkets,
-  //     icon: <FundOutlined />,
-  //   },
-  //   {
-  //     title: 'Number Of Exchanges',
-  //     value: coinData.numberOfExchanges,
-  //     icon: <MoneyCollectOutlined />,
-  //   },
-  //   {
-  //     title: 'Aprroved Supply',
-  //     value: coinData.approvedSupply ? <CheckOutlined /> : <StopOutlined />,
-  //     icon: <ExclamationCircleOutlined />,
-  //   },
-  //   {
-  //     title: 'Total Supply',
-  //     value: `$ ${millify(Number(coinData.totalSupply))}`,
-  //     icon: <ExclamationCircleOutlined />,
-  //   },
-  //   {
-  //     title: 'Circulating Supply',
-  //     value: `$ ${millify(Number(coinData.circulatingSupply))}`,
-  //     icon: <ExclamationCircleOutlined />,
-  //   },
-  // ];
+  const stats = [
+    {
+      title: 'Price to USD',
+      value: coinData.price ? millify(coinData.price) : null,
+      icon: <DollarCircleOutlined />,
+    },
+    { title: 'Rank', value: coinData.rank, icon: <NumberOutlined /> },
+    {
+      title: '24h Volume',
+      value: `$ ${coinData.volume && millify(coinData.volume)}`,
+      icon: <ThunderboltOutlined />,
+    },
+    {
+      title: 'Market Cap',
+      value: `$ ${coinData.marketCap && millify(coinData.marketCap)}`,
+      icon: <DollarCircleOutlined />,
+    },
+    {
+      title: 'All-time-high(daily avg.)',
+      value: `$ ${millify(coinData.allTimeHigh.price)}`,
+      icon: <TrophyOutlined />,
+    },
+  ];
+
+  const genericStats = [
+    {
+      title: 'Number Of Markets',
+      value: coinData.numberOfMarkets,
+      icon: <FundOutlined />,
+    },
+    {
+      title: 'Number Of Exchanges',
+      value: coinData.numberOfExchanges,
+      icon: <MoneyCollectOutlined />,
+    },
+    {
+      title: 'Aprroved Supply',
+      value: coinData.approvedSupply ? <CheckOutlined /> : <StopOutlined />,
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: 'Total Supply',
+      value: `$ ${millify(coinData.totalSupply)}`,
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: 'Circulating Supply',
+      value: `$ ${millify(coinData.circulatingSupply)}`,
+      icon: <ExclamationCircleOutlined />,
+    },
+  ];
 
   if (isFetching) return 'Still loading...';
 
   return (
     <>
-      <h1>{cryptoId} lkahsdkghksldg lkhsaklhkgs klahsgklhsg lkhsg</h1>
-      {/* <Col>
+      <h1> lkahsdkghksldg lkhsaklhkgs klahsgklhsg lkhsg</h1>
+      <Col>
         <Col>
           <Title>
             {coinData.name} {coinData.slug}
@@ -99,7 +102,7 @@ const CryptoDetails = () => {
             <Option key={i}>{t}</Option>
           ))}
         </Select>
-      </Col> */}
+      </Col>
     </>
   );
 };
